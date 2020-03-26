324b79f2793e2c85ac5ef7c4a8909217a7dea7bd
ThalysonIzzatx2 <thalyson.dev@gmail.com>
2020-03-26 01:40:54 -0300

    update

--- server.js
+++ server.js
@@ -2,18 +2,12 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser'),
-  apicache = require('apicache'),
-  cache = apicache.middleware;
+  cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cache('60 minutes'));
+app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 var routes = require('./api/routes/routes'); //importing route
 routes(app); //register the route
